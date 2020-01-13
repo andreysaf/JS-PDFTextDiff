d30f0ae91ece67e4d7663384d38c47bc15e65339
Andrey Safonov <a.so777@live.ca>
2020-01-12 19:26:32 -0800

    - Updated to use latest WebViewer 6.0.4

--- js/textdiff.js
+++ js/textdiff.js
@@ -1,58 +1,25 @@
-const loadPDFs = (firstPDF, secondPDF, local) => {
+const loadPDFs = async (firstPDF, secondPDF) => {
     CoreControls.setWorkerPath('../lib/core');
-
-    var doc1 = new CoreControls.Document('firstPDF.pdf', 'pdf');
-    var doc2 = new CoreControls.Document('secondPDF.pdf', 'pdf');
 
     var text1 = document.getElementById('text1');
     var text2 = document.getElementById('text2');
 
     const pageIndex = 0; // Extract the text in the first page
 
-    CoreControls.getDefaultBackendType().then(function (backendType) {
-        var options = {
-            workerTransportPromise: CoreControls.initPDFWorkerTransports(backendType, {}, window.sampleL /* license key here */ ),
-        };
+    const doc1 = await CoreControls.createDocument(firstPDF, {});
+    const doc2 = await CoreControls.createDocument(secondPDF, {});
 
-        var partRetriever1, partRetriever2;
-
-        if (local){
-            partRetriever1 = new CoreControls.PartRetrievers.LocalPdfPartRetriever(firstPDF);
-            partRetriever2 = new CoreControls.PartRetrievers.LocalPdfPartRetriever(secondPDF);
-        } else {
-            partRetriever1 = new CoreControls.PartRetrievers.ExternalPdfPartRetriever(firstPDF);
-            partRetriever2 = new CoreControls.PartRetrievers.ExternalPdfPartRetriever(secondPDF);
-        }
-
-
-        doc1.loadAsync(partRetriever1, function (err) {
-            if (err) {
-                return;
-            }
-
-            doc1.loadPageText(pageIndex, (text) => {
-                text1.innerHTML = text;
-              });
-        }, options);
-
-        doc2.loadAsync(partRetriever2, function (err) {
-            if (err) {
-                return;
-            }
-            doc2.loadPageText(pageIndex, (text) => {
-                text2.innerHTML = text;
-                compareText(text1.innerHTML, text2.innerHTML);
-              });
-        }, options);
-
-
+    doc1.loadPageText(pageIndex, (text) => {
+        text1.innerHTML = text;
+      })
+    doc2.loadPageText(pageIndex, (text) => {
+        text2.innerHTML = text;
+        compareText(text1.innerHTML, text2.innerHTML);
     });
 }
 
 const compareText = (text1, text2) => {
     var textDiff = document.getElementById('textdiff');
-    console.log(text1);
-    console.log(text2);
     var dmp = new diff_match_patch();
     var diff = dmp.diff_main(text1, text2);
     textDiff.innerHTML = dmp.diff_prettyHtml(diff);
@@ -64,7 +31,7 @@ window.onload = () => {
         const firstPdf = '../files/TextDiff.pdf';
         const secondPdf = '../files/TextDiffChanged.pdf';
 
-        loadPDFs(firstPdf, secondPdf, false);
+        loadPDFs(firstPdf, secondPdf);
     };
 
     document.getElementById('url-form').onsubmit = function (e) {
@@ -72,7 +39,7 @@ window.onload = () => {
         const firstPdf = document.getElementById('url').value;
         const secondPdf = document.getElementById('url2').value;
 
-        loadPDFs(firstPdf, secondPdf, false);
+        loadPDFs(firstPdf, secondPdf);
     };
 
     document.getElementById('file-picker-form').onsubmit = function (e) {
@@ -80,6 +47,6 @@ window.onload = () => {
         const firstPdf = document.forms['file-picker-form'][0].files[0];
         const secondPdf = document.forms['file-picker-form'][1].files[0];
 
-        loadPDFs(firstPdf, secondPdf, true);
+        loadPDFs(firstPdf, secondPdf);
     };
 };
